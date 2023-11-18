5600bfd4a025bfca6dc2c98cd89fc44c5eec4f95
Aways Exabyte <lzx13206010951@outlook.com>
2023-11-18 19:21:27 +0800

    VerEdit

--- CPP/Verinfo.h
+++ CPP/Verinfo.h
@@ -4,7 +4,7 @@ using namespace std;
 
 string VersionCode = "Spark-Core_Project_rebuild";
 string CodeName = "SparkCore_Build";
-string VerNotice = "SparkCore_Build_Stage_2";
+string VerNotice = "SparkCore_Build_Stage_3";
 string RunPlatfom = "Windows (MSVC 64Bit)";//Must Include Windows/Linux one
 
 int VerCode = 4103;
