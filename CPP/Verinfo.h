ae5028dc85fe46d070465b5227fecb28ecd8dfb1
Aways Exabyte <lzx13206010951@outlook.com>
2023-11-26 12:07:39 +0800

    PreRelease

--- CPP/Verinfo.h
+++ CPP/Verinfo.h
@@ -7,7 +7,7 @@ using namespace std;
 
 string VersionCode = "Spark-Core_Developer_Preview";
 string CodeName = "SparkCore_Pre-Release";
-string VerNotice = "SparkCore_PreRelease";
+string VerNotice = "SparkCore_Dev-Pre-Release";
 string RunPlatfom = "Windows (MSVC 64Bit)";//Must Include Windows/Linux one
 
 int VerCode = 4105;
